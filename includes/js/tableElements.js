68542cebc5b95481b2ce3851d54f3ef049367afc
Jason Gillman Jr <jgillman@liquidweb.com>
2014-01-14 13:27:37 -0500

    Added ability to remove symbols and propositions. Also removed the 'available symbols' buttons

--- includes/js/tableElements.js
+++ includes/js/tableElements.js
@@ -1,20 +1,32 @@
 function addSymbol(charCode)
 {
-	workingChar = String.fromCharCode(charCode);
-	$('#addSymbol').remove();
-	$('#symbolForm').append('<div class="symbol" data-symbol="' + workingChar + '">' + workingChar + ': <input type="text" class="symbolRep" name="' + workingChar + '" onchange="updateButton(\'' + workingChar + '\');"><a id="addSymbol" href="#" onclick="addSymbol(' + (charCode + 1) + ');">+</a></div>');
-	$('#availableSymbols').append('<input type="button" id="symbolButton' + workingChar + '" class="symbolButton" value="' + workingChar +'">');
+	workingChar	=	String.fromCharCode(charCode); // The next character
+	$('#symbolAdder').remove();
+	$('#symbolRemover').remove();
+	$('#symbolForm').append('<div class="symbol" data-symbol="' + workingChar + '">' + workingChar + ': <input type="text" class="symbolRep" name="' + workingChar + '" onchange="updateButton(\'' + workingChar + '\');"><input type="button" id="symbolAdder" value="+" onclick="addSymbol(' + (charCode + 1) + ');"><input type="button" id="symbolRemover" value="-" onclick="remSymbol(' + (charCode) + ');"></div>');
+}
+
+function remSymbol(charCode)
+{
+	workingChar =	String.fromCharCode(charCode); // The current character
+	prevChar	=	String.fromCharCode(charCode - 1);
+	$('[data-symbol=' + workingChar + ']').remove();
+	$('[data-symbol=' + prevChar + ']').append('<input type="button" id="symbolAdder" value="+" onclick="addSymbol(' + (charCode) + ');"><input type="button" id="symbolRemover" value="-" onclick="remSymbol(' + (charCode - 1) + ');">');
 }
 
 function addProposition(propNumber)
 {
-	$('#addProposition').remove();
-	$('#propositionForm').append('<div class="propLine" data-proposition="' + propNumber + '">Proposition ' + propNumber + ': <input type="text" class="proposition" name="' + propNumber + '"><a id="addProposition" href="#" onclick="addProposition(' + (propNumber + 1) + ');">+</a></div>');
+	// The next proposition
+	$('#propAdder').remove();
+	$('#propRemover').remove();
+	$('#propositionForm').append('<div class="propLine" data-proposition="' + propNumber + '">Proposition ' + propNumber + ': <input type="text" class="proposition" name="' + propNumber + '"><input type="button" id="propAdder" value="+" onclick="addProposition(' + (propNumber + 1) + ');"><input type="button" id="propRemover" value="-" onclick="remProposition(' + (propNumber) + ');"></div>');
 }
 
-function updateButton(character)
+function remProposition(propNumber)
 {
-	$('#symbolButton' + character).attr('title', $('[name="' + character + '"]').val());
+	// propNumber is the current proposition that is being removed
+	$('[data-proposition=' + propNumber + ']').remove();
+	$('[data-proposition=' + (propNumber - 1) + ']').append('<input type="button" id="propAdder" value="+" onclick="addProposition(' + (propNumber) + ');"><input type="button" id="propRemover" value="-" onclick="remProposition(' + (propNumber - 1) + ');">');
 }
 
 function getSymbols()
@@ -67,22 +79,16 @@ function analyze()
 
 function generateTable(analysis)
 {
-	//tableColumns = new Array();
-	//tableData = new Array();
-
 	// Set columns
-	//tableColumns.push('Truthset');
 	$('#resultTable').empty(); // Cleanout the table if it already exists
 	tableHtml = '<tr>' + "\n" + '<th>Truthset</th>' + "\n";
 	var i;
 	for(i = 0; i <= (availableSymbols.length - 1); ++i)
 	{
-		//tableColumns.push(availableSymbols[i]);
 		tableHtml += '<th>' + availableSymbols[i] + '</th>' + "\n";
 	}
 	for(i = 0; i <= (analysis.byProp.length - 1); ++i)
 	{
-		//tableColumns.push(analysis.byProp[i].proposition);
 		tableHtml += '<th>' + analysis.byProp[i].proposition + '</th>' + "\n";
 	}
 	tableHtml += '</tr>' + "\n";
@@ -90,20 +96,16 @@ function generateTable(analysis)
 	// Set data
 	for(i = (Object.keys(analysis.byInt).length - 1); i >= 0; --i) // Counting down, since we want to start with all true
 	{
-		//tableRow = new Array();
 		tableHtml += '<tr>' + "\n" + '<td>' + i + '</td>' + "\n";
-		//tableRow.push(i); // The truthset (the integer version of the boolean bits)
 		var j;
 		for(j = 0; j <= (availableSymbols.length - 1); ++j) // Push in the symbols
 		{
 			if(analysis.byInt[i].truthValues[availableSymbols[j]])
 			{
-				//tableRow.push('T');
 				tableHtml += '<td>T</td>' + "\n";
 			}
 			else
 			{
-				//tableRow.push('F');
 				tableHtml += '<td>F</td>' + "\n";
 			}
 		}
@@ -111,16 +113,13 @@ function generateTable(analysis)
 		{
 			if(analysis.byInt[i].propositions[j].propositionValue)
 			{
-				//tableRow.push('T');
 				tableHtml += '<td>T</td>' + "\n";
 			}
 			else
 			{
-				//tableRow.push('F');
 				tableHtml += '<td>F</td>' + "\n";
 			}
 		}
-		//tableData.push(tableRow);
 		tableHtml += '</tr>' + "\n";
 	}
 
